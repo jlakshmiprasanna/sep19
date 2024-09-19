d5704d3886028ab7f9ed86f0d253b93ff1786a0f
Jonnalagadda Lakshmi Prasanna <142923910+jlakshmiprasanna@users.noreply.github.com>
2024-09-19 21:00:35 +0530

    Add files via upload

---
+++ task.js
@@ -0,0 +1,64 @@
+//(Data Types)
+
+// (PRIMITIVE-->NUMBERS)
+var age=22;
+
+// (PRIMITIVE-->STRING)
+var name="Prasanna";
+var job="Trainer&Developer";
+
+// (PRIMITIVE-->BOOLEAN)
+var value="true";
+var sal=30000;
+
+console.log(age);
+console.log(name);
+console.log(job);
+console.log(value);
+console.log(sal);
+
+// (NON-PRIMITIVE-->OBJECT)
+var details={
+    name:"JLP",
+    age:22,
+}
+console.log(details.name)
+
+// (NON-PRIMITIVE-->ARRAYS)
+var info=[4,"JLP","true",{name:"JLP",age:22},[1,2,3]]
+
+console.log(info);
+console.log(info[5-1]);
+
+            // (DATA TYPES)
+
+//(DATA TYPES-var)
+
+var d=10;
+d1="RRR";
+d2=75;
+console.log(d);
+console.log(d1);
+console.log(d2);
+
+//(DATA TYPES-let)
+let e=20;
+console.log(e);
+
+//(DATA TYPES-const)
+const f=30;
+console.log(f);
+
+//(DATA TYPES-all)
+var g="Prassu";
+let g1=2002;
+const g2="True";
+{
+    let h=2003;
+    console.log(h);
+    console.log(g);
+}
+{
+    const i=2004;
+    console.log(i);
+}
